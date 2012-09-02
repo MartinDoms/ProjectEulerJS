e6a14d483f6dab4718cda120c4a89378ec4f89c7
Martin Doms <iamaelephant@gmail.com>
2012-09-02 21:03:47 +1200

    fixed 007

--- p007.js
+++ p007.js
@@ -1,34 +1,22 @@
-function sumOfSquares(x) {
-  return ((2*x*x*x) + (3*x*x) + x)/6;
+// Sieve of Eratosthenes
+function prime(n) {
+  var max = 500000;
+  var currN = 0;
+  var currP = -1;
+  var nums = [];
+  for (var i = 2; i <= max; i++) {
+    if (!nums[i]) {// prime
+      currN++;
+      currP = i;
+      if (currN == n) return currP;
+      for (var j = i*2; j < max; j+=i) {
+        nums[j] = true;
+      }
+    }
+  }
 }
 
-function  squareOfSum(x) {
-  var sum = (x * (x + 1))/2;
-  return sum*sum;
-}
-
-function sumDiff(x) {
-  return squareOfSum(x) - sumOfSquares(x);
-}
-
-test("sumOfSquares", function() {
-  equal(sumOfSquares(1), 1);
-  equal(sumOfSquares(2), 5);
-  equal(sumOfSquares(3), 14);
-  equal(sumOfSquares(10), 385);
-});
-
-test("squareOfSum", function() {
-  equal(squareOfSum(1), 1);
-  equal(squareOfSum(2), 9);
-  equal(squareOfSum(3), 36);
-  equal(squareOfSum(10), 3025);
-});
-
-test("sumDiff", function() {
-  equal(sumDiff(1), 0);
-  equal(sumDiff(2), 4);
-  equal(sumDiff(3), 22);
-  equal(sumDiff(10), 2640);
-  equal(sumDiff(100), 25164150);
+test("prime", function() {
+  equal(prime(6), 13);
+  equal(prime(10001), 104743);
 });
