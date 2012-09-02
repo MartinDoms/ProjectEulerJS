3dba1bdc90538db97cc6a56f5bb0c3a38a8bc2b4
Martin Doms <iamaelephant@gmail.com>
2012-09-02 21:06:48 +1200

    fixed p015

--- p015.js
+++ p015.js
@@ -1,41 +1,16 @@
-var mem = [];
-
-function chainLengthRec(n) {
-  if (mem[n]) return mem[n];
-  var result = 1;
-  if (n == 1) return result;
-  if (n%2==0) result += chainLengthRec(n/2);
-  else result += chainLengthRec(3*n+1);
-  
-  mem[n] = result;
-  return result;
+function factorial(n) {
+  var r = 1;
+  for (var i = 1; i <= n; i++) r *= i;
+  return r;
 }
 
-function chainLength(n) {
-  return chainLengthRec(n, 1);
+function routes(size) {
+  var r = 1;
+  for (var i = size+1; i <= size*2; i++) r *= i;
+  return r/factorial(size);
 }
 
-function longestChain(n) {
-  var longest = 0;
-  var num = n-1;
-  for (var i = n-1; i > 0; i--) {
-    var chain = chainLength(i);
-    if (chain > longest) {
-      longest = chain;
-      num = i;
-    }
-  }
-  return num;
-}
-
-test("longestChain", function() {
-  equal(longestChain(1000000), 837799);
-});
-
-test("chainLength", function() {
-  equal(chainLength(13), 10);
-  equal(chainLength(2), 2);
-  equal(chainLength(3), 8);
-  equal(chainLength(4), 3);
-  equal(chainLength(5), 6);
+test("routes", function() {
+  equal(routes(2), 6);
+  equal(routes(20), 137846528820);
 });
